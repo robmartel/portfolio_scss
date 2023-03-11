a22b5057cca3c263551f7b3e88ccfa726b1e9289
robmartel <99683835+robmartel@users.noreply.github.com>
2023-03-11 15:57:44 -0500

    Completed the navbar and mobile navbar and styled to make responsive, used framer motion for special effects and react icons for the hamburger menu and close icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './Navbar.scss';
+
+import { navLinks } from '../../Data';
+import { socialIcons } from '../../Data';
+
+import { motion } from 'framer-motion';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 
 const Navbar = () => {
-  return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  )
-}
+  const [scroll, setScroll] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  const menuVariants = {
+    hidden: {
+      scale: 0,
+    },
+    visible: {
+      scale: 50,
+      transition: {
+        type: 'tween',
+        duration: 0.5,
+      },
+    },
+  };
+  const navLinkVariants = {
+    hidden: {
+      display: 'none',
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      y: -30,
+      transition: {
+        delay: 0.7,
+      },
+    },
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScroll(window.scrollY > 20);
+    });
+  }, []);
+
+  return (
+    <motion.div
+      initial={{ y: -25 }}
+      animate={{ y: -5 }}
+      transition={{ duration: 0.5 }}
+      className={scroll ? 'header active' : 'header'}
+    >
+      <div className='Nav_container'>
+        <div className='logo'>
+          <h3>RM</h3>
+        </div>
+        <ul className='nav_links'>
+          {navLinks.map((navlink, index) => {
+            return (
+              <li key={index}>
+                <a href={`#${navlink}`}>{navlink}</a>
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* Social Media Icons */}
+        <div className='social_icons'>
+          {socialIcons.map((socialIcon, index) => {
+            return <div key={index}>{socialIcon}</div>;
+          })}
+        </div>
+
+        {/* Mobile Menu */}
+        <div className='menu'>
+          <HiMenuAlt4
+            onClick={() => {
+              setToggle(true);
+            }}
+          />
+        </div>
+        <motion.div
+          className='closeMenu'
+          variants={menuVariants}
+          initial='hidden'
+          animate={toggle ? 'visible' : 'hidden'}
+        ></motion.div>
+
+        <motion.div
+          variants={navLinkVariants}
+          animate={toggle ? 'visible' : 'hidden'}
+          className='menuX'
+        >
+          <HiX onClick={() => setToggle(false)} />
+          {navLinks.map((navlink, index) => {
+            return (
+              <li key={index}>
+                <a
+                  href={`#${navlink}`}
+                  onClick={() => setToggle(false)}
+                >
+                  {navlink}
+                </a>
+              </li>
+            );
+          })}
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Navbar;
