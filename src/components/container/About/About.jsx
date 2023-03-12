be86a46cbbb0e8dd485776f2438d5f2770f9aeff
robmartel <99683835+robmartel@users.noreply.github.com>
2023-03-12 11:12:06 -0400

    added motion effects to both home and about links

--- src/components/container/About/About.jsx
+++ src/components/container/About/About.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
-import './About.scss'
+import React from 'react';
+import './About.scss';
 
-import { bios } from '../../../Data'
-
-import portfolio from '../../../assets/portfolio.jpg'
+import { bios } from '../../../Data';
+import portfolio from '../../../assets/portfolio.jpg';
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
-    <div className="container" id="about">
-      <div className="title">
+    <div
+      className='container'
+      id='about'
+    >
+      <div className='title'>
         <span>Who Am I?</span>
         <h1>About Me</h1>
       </div>
-      <div className="about_container">
-        <div className="about_left">
-          <img src={portfolio} alt="about_image" />
+      <div className='about_container'>
+        <div className='about_left'>
+          <img
+            src={portfolio}
+            alt='about_image'
+          />
         </div>
-          <div className="about_right">
-            <p>
-              This is where you write a bunch of stuff about me. 
-              Thing like you love walks in the rain, and rainbows and 
-              lollipops. This is the place where all the great stuff
-              is going to be written, and then everyone will love you.
-            </p>
-            {bios.map((bio) => {
-              return (
-                <div className="bio" key={bio.id}>
-                  <span className="biokey">{bio.icon}{bio.key}</span>
-                  <span className="bioValue">{bio.value}</span>
-                </div>
-              )
-            })}
-            <a href="#" download="">Download Resume</a>
-          </div>
+        <div className='about_right'>
+          <p>
+            This is where you write a bunch of stuff about me. Thing like you
+            love walks in the rain, and rainbows and lollipops. This is the
+            place where all the great stuff is going to be written, and then
+            everyone will love you.
+          </p>
+          {bios.map((bio) => {
+            return (
+              <div
+                className='bio'
+                key={bio.id}
+              >
+                <span className='biokey'>
+                  {bio.icon}
+                  {bio.key}
+                </span>
+                <span className='bioValue'>{bio.value}</span>
+              </div>
+            );
+          })}
+          <motion.a
+            href='#'
+            download=''
+            whileHover={{scale: 1.1}}
+            transition={{duration: 0.3}}
+          >
+            Download Resume
+          </motion.a>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/container/Home/Home.jsx
+++ src/components/container/Home/Home.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import './Home.scss'; 
+import './Home.scss';
+
 import portfolio from '../../../assets/portfolio.jpg';
+import { motion } from 'framer-motion';
+
 const Home = () => {
   return (
     <div
       className='container'
       id='home'
     >
-      <div className="profile">
-      <img src={portfolio} alt="portfolio-image" />
+      <div className='profile'>
+        <img
+          src={portfolio}
+          alt='portfolio-image'
+        />
       </div>
-      <div className="profile_text">
-        <h3 className="name">Hey, I'm <span>Rob Martel</span></h3>
-      <span className="job">a Full Stack Web Developer</span>
-      <span className="text">Passionate <br /> to create innovative <br /> web products.</span>
-      <a href="contact">Connect with me</a>
-      <div className="web">
-        Web Developer
+      <div className='profile_text'>
+        <h3 className='name'>
+          Hey, I'm <span>Rob Martel</span>
+        </h3>
+        <span className='job'>a Full Stack Web Developer</span>
+        <span className='text'>
+          Passionate <br /> to create innovative <br /> web products.
+        </span>
+        <motion.a
+          href='contact'
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+        >
+          Connect with me
+        </motion.a>
+        <div className='web'>Web Developer</div>
+        <div className='freelance'>Freelancer</div>
+        <div className='fullstack'>Full Stack</div>
       </div>
-      <div className="freelance">Freelancer</div>
-      <div className="fullstack">Full Stack</div>
-    </div>
     </div>
   );
 };
